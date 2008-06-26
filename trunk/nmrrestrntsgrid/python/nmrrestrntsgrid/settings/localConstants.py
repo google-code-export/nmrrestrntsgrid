efd219ef0fad2f542e8a188457a0885bdfa9b5ec
jurgenfd <jurgenfd@028c42cb-184d-0410-bf56-194c7f083267>
2008-06-26 08:09:37 +0000

    Adapted star 2 ccpn script from Wim. Needs debugging.

--- trunk/nmrrestrntsgrid/python/nmrrestrntsgrid/settings/localConstants.py
+++ trunk/nmrrestrntsgrid/python/nmrrestrntsgrid/settings/localConstants.py
@@ -4,11 +4,13 @@ import os
 # Needs to map exactly to settings.csh 
 
 user_dir         = os.getenv('UJ')
+ccpn_tmp_dir     = os.getenv('ccpn_tmp_dir')
 scripts_dir      = os.getenv('scripts_dir')
 workspace        = os.path.join(user_dir,'workspace')
 big_dir          = os.path.join(user_dir,'NRG')
 results_dir      = os.path.join(big_dir,'Results')
     
 urlDB2 = None
+
 print "DEBUG: using user_dir: [%s] " % user_dir
 print "Read nmrresrntsgrid.settings.localConstants.py version 0.3.1"
