057ebd4da6f4f506fde7992c06ab88df17ffabf8
wfvranken <wfvranken@028c42cb-184d-0410-bf56-194c7f083267>
2008-06-26 09:19:39 +0000

    Now sets mapping automatically. If this still causes problems, can still modify mapping routine to make sure it doesn't.

--- nmrrestrntsgrid/python/nmrrestrntsgrid/util/starToCcpn.py
+++ nmrrestrntsgrid/python/nmrrestrntsgrid/util/starToCcpn.py
@@ -1,60 +1,73 @@
 """
 File requested by Jurgen to read in NMR-STAR file and create a CCPN project.
-
-Execute like:
-python -u $dir_nrg_python/nmrrestrntsgrid/util/starToCcpn.py [in out]
 """
-from ccpnmr.format.converters.NmrStarFormat import NmrStarFormat
+
+import os, shutil
+
 from memops.api import Implementation
+from ccpnmr.format.converters.NmrStarFormat import NmrStarFormat
+from ccpnmr.format.process.matchResonToMolSys import matchResonToMolSys
+
 from memops.general.Io import saveProject
-from nmrrestrntsgrid.settings.localConstants import ccpn_tmp_dir
-import sys
-import os
-import shutil
-
-
-#allChemCompDataPath = '/Users/wim/workspace/stable/all/data/allChemComps'
-allChemCompDataPath = os.path.join(ccpn_tmp_dir, 'data/allChemComps')
-fcInputDir          = os.path.join(ccpn_tmp_dir, 'data/archives/bmrb/nmrRestrGrid')
 
 if __name__ == '__main__':
-    ccpnProjectPath     = "test"
-    ccpnProjectName     = '1a4d'  
-    inputNmrStarFile    = os.path.join(fcInputDir, ccpnProjectName, 'joinedCoord.str')
-    ccpnProjectSavePath = os.path.join(ccpnProjectPath, ccpnProjectName)
 
-    if len(sys.argv)>1:
-        inputNmrStarFile = sys.argv[1]
-        ccpnProjectSavePath = sys.argv[2]
+  inputNmrStarFile = '/Users/wim/workspace/stable/all/data/archives/bmrb/nmrRestrGrid/1a4d/joinedCoord.str'
+
+  ccpnProjectPath = "test"
+  ccpnProjectName = '1a4d'  
+  ccpnProjectSavePath = os.path.join(ccpnProjectPath,ccpnProjectName)
+
+  allChemCompDataPath = '/Users/wim/workspace/stable/all/data/allChemComps'
   
-    # Create project
-    ccpnProject = Implementation.MemopsRoot(name = ccpnProjectName)
-    
-    # Read NMR-STAR file
-    nmrStarFormat = NmrStarFormat(ccpnProject, guiParent = None)
-    entry = nmrStarFormat.readProject(inputNmrStarFile, version = '3.1', minimalPrompts = True, 
-                useOriginalChainCode = True, linkAtoms = False, chemCompPath = allChemCompDataPath)
-    
-    # Run linkResonances
-    keywds = {}    
-    strucGen = entry.findFirstStructureGeneration()
-    
-    keywds['globalStereoAssign']    = True
-    keywds['nmrConstraintStore']    = strucGen.nmrConstraintStore
-    keywds['setSingleProchiral']    = True
-    keywds['setSinglePossEquiv']    = True
-    keywds['minimalPrompts']        = True
-    keywds['useCommonNames']        = False
-    keywds['useAmbiguity']          = True
-    keywds['useLinkResonancePopup'] = False
-    keywds['useEmptyNamingSystems'] = False
-    
-    nmrStarFormat.linkResonances(**keywds)
-    
-    # Write out the CCPN project
-    # Will remove existing project!
-    if os.path.exists(ccpnProjectSavePath):
-      shutil.rmtree(ccpnProjectSavePath)
-    
-    saveProject(ccpnProject, newPath = ccpnProjectSavePath)
-    
+  #
+  # Create project
+  #
+  
+  ccpnProject = Implementation.MemopsRoot(name = ccpnProjectName)
+  
+  #
+  # Read NMR-STAR file
+  #
+  
+  nmrStarFormat = NmrStarFormat(ccpnProject, guiParent = None)
+  entry = nmrStarFormat.readProject(inputNmrStarFile, version = '3.1', minimalPrompts = True, useOriginalChainCode = True, linkAtoms = False, chemCompPath = allChemCompDataPath)
+
+  #
+  # Run linkResonances
+  #   
+  
+  keywds = {}
+  
+  strucGen = entry.findFirstStructureGeneration()
+  resonances = strucGen.nmrConstraintStore.sortedFixedResonances()
+ 
+  keywds['globalStereoAssign'] = True
+  keywds['nmrConstraintStore'] = strucGen.nmrConstraintStore
+  keywds['setSingleProchiral']    = True
+  keywds['setSinglePossEquiv']    = True
+  keywds['minimalPrompts']        = True
+  keywds['useCommonNames']        = False
+  keywds['useAmbiguity']          = True
+  keywds['useLinkResonancePopup'] = False
+  keywds['useEmptyNamingSystems'] = False
+  
+  # Use automapping in this case
+  forceChainMappings = matchResonToMolSys(resonances,ccpnProject.findFirstMolSystem(),assignFormat = 'nmrStar')
+
+  if forceChainMappings:
+    keywds['forceChainMappings'] = forceChainMappings
+
+
+  nmrStarFormat.linkResonances(**keywds)
+  
+  #
+  # Write out the CCPN project
+  #
+  # Will remove existing project!
+  # 
+  
+  if os.path.exists(ccpnProjectSavePath):
+    shutil.rmtree(ccpnProjectSavePath)
+
+  saveProject(ccpnProject, newPath = ccpnProjectSavePath)
